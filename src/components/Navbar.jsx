85a509f78a5433990a4726be450232e7cc6a475e
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2022-04-23 13:29:17 +0530

    main

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,90 @@
+// import { Link } from "react-router-dom";
+import React from "react";
+import "./Navbar.css";
+
+export const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="subnav">
+                <div className="subnavdiv">Home</div>
+                <div className="subnav-content">
+                    <div className="grid">
+                        <a>Company</a> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                    </div>
+                </div>
+            </div>
+            <div className="subnav">
+                <div className="subnavdiv">About</div>
+                <div className="subnav-content">
+                <div className="grid">
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                    </div>
+                </div>
+            </div>
+            <div className="subnav">
+                <div className="subnavdiv">Services</div>
+                <div className="subnav-content">
+                <div className="grid">
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                    </div>
+                </div>
+            </div>
+            <div className="subnav">
+                <div className="subnavdiv">Partners</div>
+                <div className="subnav-content">
+                <div className="grid">
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                    </div>
+                </div>
+            </div>
+            <div className="subnav">    
+                <div className="subnavdiv">Contact</div>
+                <div className="subnav-content">
+                <div className="grid">
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                        <div>Company</div> 
+                        <div>team</div> 
+                        <div>Carerers</div>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    )
+}
